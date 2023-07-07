bb01597ad86e4d92a4f213b2c3cf602898c6520f
Randeep Singh <randeepsingh986@gmail.com>
2023-07-07 18:42:55 +0530

    Added backend

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-require("dotenv").config();
 const express = require("express");
-const app = express();
+require("dotenv").config();
 const {connection}=require("./db/conn");
 const router = require("./routes/router");
 const cors = require("cors");
 const cookiParser = require("cookie-parser")
 const port = 8009;
+const app = express();
 
 app.use(express.json());
 app.use(cookiParser());
 app.use(cors());
 app.use(router);
-
+app.get("/",(req,res)=>{
+    res.send("Welcome")
+})
 
 app.listen(port, async ()=>{
     try{
