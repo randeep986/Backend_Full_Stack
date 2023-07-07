de3f17c2c8848a2c7e8d0cba17238140e198337f
Randeep Singh <randeepsingh986@gmail.com>
2023-07-07 18:26:14 +0530

    Added backend

--- db/conn.js
+++ db/conn.js
@@ -2,9 +2,12 @@ const mongoose = require("mongoose");
 
 const DB = process.env.DATABASE
 
-mongoose.connect(DB,{
+ const connection= mongoose.connect(DB,{
     useUnifiedTopology: true,
     useNewUrlParser: true
 }).then(()=> console.log("DataBase Connected")).catch((errr)=>{
     console.log(errr);
 })
+module.exports={
+    connection
+}
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-require("./db/conn");
+const {connection}=require("./db/conn");
 const router = require("./routes/router");
 const cors = require("cors");
 const cookiParser = require("cookie-parser")
@@ -13,6 +13,14 @@ app.use(cors());
 app.use(router);
 
 
-app.listen(port,()=>{
+app.listen(port, async ()=>{
+    try{
+        await connection;
+        console.log("Connected to DB succesfull")
+    }
+    catch(err){
+        console.log("error connecting to db")
+        console.log(err)
+    }
     console.log(`server start at port no : ${port}`);
 })
